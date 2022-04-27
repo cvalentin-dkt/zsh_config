3933d27678e16248357765c0fe2fbe13136f9317
Clément VALENTIN <clement.valentin.partner@decathlon.com>
2022-04-27 15:21:38 +0200

    init

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# CLEAN
+rm -Rf ~/.oh-my-zsh/ ~/.zgen/ ~/.zshrc
+
+sudo apt update
+sudo apt install -y tmux zsh git software-properties-common grc
+#sudo add-apt-repository ppa:fontforge/fontforge
+#sudo apt-get update
+#sudo apt-get install -y fontforge
+
+# FONT
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd -
+
+# OH MY ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# ZGEN
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+
+# OH MY ZSH
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# OH MY TMUX
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+cd -
+
+# COPY MY CONFIG
+cp .zshrc ~/
+cp .tmux.conf ~/
+cp .tmux.local.conf ~/
+mkdir -p ~/.zshrc_profiles
+
+# DEFAULT BASH
+sudo usermod --shell /usr/bin/zsh $USER
+
+zsh
